35c88970594a78041748539ee29f224cb974fa58
Manuel Pereira <manilex2@gmail.com>
2022-03-01 18:36:52 -0500

    Commit inicial

--- AWS/index.js
+++ AWS/index.js
@@ -7,17 +7,18 @@ const auth = new google.auth.GoogleAuth({
     scopes: 'https://www.googleapis.com/auth/spreadsheets'
 });
 const { database } = require('./keys');
-const conexion = mysql.createConnection({
-    host: database.host,
-    user: database.user,
-    password: database.password,
-    port: database.port,
-    database: database.database
-});
-var ticker = [];
 
 exports.handler = async function (event) {
     const promise = new Promise(async function() {
+        const conexion = mysql.createConnection({
+            host: database.host,
+            user: database.user,
+            password: database.password,
+            port: database.port,
+            database: database.database
+        });
+        var cripto = [];
+        var propost = [];
         const spreadsheetId = process.env.SPREADSHEET_ID;
         const client = await auth.getClient();
         const googleSheet = google.sheets({ version: 'v4', auth: client });
@@ -26,37 +27,64 @@ exports.handler = async function (event) {
             spreadsheetId,
             range: `${process.env.ID_HOJA}!A2:L`
         })).data
+        
         for (let i = 0; i < request.values.length; i++) {
             for (let x = 0; x < request.values[i].length; x++) {
-                ticker.push(request.values[i][0]);
+                if(request.values[i][4] != undefined) {
+                    cripto.push(request.values[i][0]);
+                    propost.push(request.values[i][4]);
+                }
             }
         }
     
-        var tickerSinDuplicados = [...new Set(ticker)];
+        var criptoSinDuplicados = [...new Set(cripto)];
+        var criptoTransformados = criptoSinDuplicados.join(',');
     
-        mostrarDatos(tickerSinDuplicados);
-    });
-
-    async function mostrarDatos(tickerSinDuplicados) {
-            for (let i = 0; i < tickerSinDuplicados.length; i++) {
-                var tickerN = tickerSinDuplicados[i];
-                await fetch(`https://cloud.iexapis.com/stable/stock/${tickerN}/quote?token=${process.env.TOKEN_IEXAPIS}`).then((res)=>{
-                    return res.json();
-                }).then((json)=>{
-                    var ultimoPrecio = json;
-                    actualizarDatos(ultimoPrecio);
-                })
+        mostrarDatos(criptoTransformados);
+        //mostrarDatos(cripto, propost);
+    
+        async function mostrarDatos(criptoTransformados) {
+            await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${criptoTransformados}&vs_currencies=usd`).then((res)=>{
+                return res.json();
+            }).then((json)=>{
+                var ultimoPrecio = json;
+                actualizarDatos(ultimoPrecio);
+            })
+        }
+    
+        /* async function mostrarDatos(cripto, propost) {
+            for (let i = 0; i < cripto.length; i++) {
+                var criptoN = cripto[i];
+                var ultimoPrecio = propost[i];
+                actualizarDatos(criptoN, ultimoPrecio);
             }
-    }
-    async function actualizarDatos(ultimoPrecio) {
+        } */
+    
+        async function actualizarDatos(ultimoPrecio) {
             const time = new Date();
             const month = time.getMonth() + 1;
             let fechaActual = time.getFullYear() + '-' + month + '-' + time.getDate();
-            var sql = `UPDATE datos SET propost = ${ultimoPrecio.latestPrice} WHERE ticker = '${ultimoPrecio.symbol}' AND fecha = '${fechaActual}'`;
-            conexion.query(sql, function (err) {
-                if (err) throw err;
-            });
-            console.log(`Se actualizo el precio actual de ${ultimoPrecio.symbol}`);
-    }
+            let criptoNew = [];
+            for (const cripto in ultimoPrecio) {
+                for (const precio in ultimoPrecio[cripto]) {
+                    criptoNew.push([cripto, ultimoPrecio[cripto][precio]]);
+                }
+            }
+            //let fechaActual = "2021-11-24";
+            for (let i = 0; i < criptoNew.length; i++) {
+                let sql = `UPDATE datos SET propost = ${criptoNew[i][1]} WHERE cripto = '${criptoNew[i][0]}' AND fecha = '${fechaActual}'`;
+                //var sql = `UPDATE datos SET propost = ${ultimoPrecio} WHERE cripto = '${criptoN}' AND fecha = '${fechaActual}'`;
+                conexion.query(sql, function (err) {
+                    if (err) throw err;
+                });
+                console.log(`Se estableció el precio ${criptoNew[i][1]} del ${fechaActual} de ${criptoNew[i][0]}`);
+            }
+            await finalizarEjecucion();
+        }
+        async function finalizarEjecucion() {
+            conexion.end();
+            respuesta.send("Ejecutado");
+        }
+    });
     return promise;
 };
---
+++ SERVER/index.js
@@ -0,0 +1,97 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const mysql = require('mysql2');
+const fetch = require('node-fetch');
+const PUERTO = 4300;
+const {google} = require('googleapis');
+const auth = new google.auth.GoogleAuth({
+    keyFile: 'credentials.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets'
+});
+const { database } = require('./keys');
+
+app.use(morgan("dev"));
+    
+app.get('/', async (solicitud, respuesta) => {
+    const conexion = mysql.createConnection({
+        host: database.host,
+        user: database.user,
+        password: database.password,
+        port: database.port,
+        database: database.database
+    });
+    var cripto = [];
+    var propost = [];
+    const spreadsheetId = process.env.SPREADSHEET_ID;
+    const client = await auth.getClient();
+    const googleSheet = google.sheets({ version: 'v4', auth: client });
+    const request = (await googleSheet.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: `${process.env.ID_HOJA}!A2:L`
+    })).data
+    
+    for (let i = 0; i < request.values.length; i++) {
+        for (let x = 0; x < request.values[i].length; x++) {
+            if(request.values[i][4] != undefined) {
+                cripto.push(request.values[i][0]);
+                propost.push(request.values[i][4]);
+            }
+        }
+    }
+
+    var criptoSinDuplicados = [...new Set(cripto)];
+    var criptoTransformados = criptoSinDuplicados.join(',');
+
+    mostrarDatos(criptoTransformados);
+    //mostrarDatos(cripto, propost);
+
+    async function mostrarDatos(criptoTransformados) {
+        await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${criptoTransformados}&vs_currencies=usd`).then((res)=>{
+            return res.json();
+        }).then((json)=>{
+            var ultimoPrecio = json;
+            actualizarDatos(ultimoPrecio);
+        })
+    }
+
+    /* async function mostrarDatos(cripto, propost) {
+        for (let i = 0; i < cripto.length; i++) {
+            var criptoN = cripto[i];
+            var ultimoPrecio = propost[i];
+            actualizarDatos(criptoN, ultimoPrecio);
+        }
+    } */
+
+    async function actualizarDatos(ultimoPrecio) {
+        const time = new Date();
+        const month = time.getMonth() + 1;
+        let fechaActual = time.getFullYear() + '-' + month + '-' + time.getDate();
+        let criptoNew = [];
+        for (const cripto in ultimoPrecio) {
+            for (const precio in ultimoPrecio[cripto]) {
+                criptoNew.push([cripto, ultimoPrecio[cripto][precio]]);
+            }
+        }
+        //let fechaActual = "2021-11-24";
+        for (let i = 0; i < criptoNew.length; i++) {
+            let sql = `UPDATE datos SET propost = ${criptoNew[i][1]} WHERE cripto = '${criptoNew[i][0]}' AND fecha = '${fechaActual}'`;
+            //var sql = `UPDATE datos SET propost = ${ultimoPrecio} WHERE cripto = '${criptoN}' AND fecha = '${fechaActual}'`;
+            conexion.query(sql, function (err) {
+                if (err) throw err;
+            });
+            console.log(`Se estableció el precio ${criptoNew[i][1]} del ${fechaActual} de ${criptoNew[i][0]}`);
+        }
+        await finalizarEjecucion();
+    }
+    async function finalizarEjecucion() {
+        conexion.end();
+        respuesta.send("Ejecutado");
+    }
+});
+
+app.listen(process.env.PORT || PUERTO, () => {
+    console.log(`Escuchando y ejecutando en el puerto: ${process.env.PORT || PUERTO}`);
+});
